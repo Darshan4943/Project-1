83f12ced1c4b157300fd1e7f8df68fd51c997ef6
KamalPanwar <kamalpanwar193@gmail.com>
2023-04-18 19:15:27 +0530

    "pictures and css added in aboutus"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import { Home } from './Components/Home/Home';
 import { Login } from './Components/Login/Login';
 import { AboutUs } from './Components/Aboutus/AboutUs';
