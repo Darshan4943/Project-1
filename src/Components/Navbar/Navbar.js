5e80a4ff2b6e937ba68cedc20003259d42c8449f
Darshan <darshanshinde4943@gmaul.com>
2023-04-20 03:19:28 +0530

    updated data

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,28 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 import { Link } from 'react-router-dom'
-import  styles  from './Navbar.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark,faBars} from '@fortawesome/free-solid-svg-icons';
+import styles from'./Navbar.module.css'
 
-export const Navbar = () => {
+
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
   return (
+    <div>
+        <header className={styles.header}>
 
-    <div className={styles.container}>
-      <header>
-        <h1><Link to='/'>Gold Gym</Link></h1>
-        <nav>
-          <ul>
+          <h1><Link to='/'>Gold </Link></h1>
+          
+            <ul className={isMenuOpen ? styles.listresponsive : styles.lists}>
 
-            <li><Link to='/aboutUs'>AboutUs</Link></li>
-            <li><Link to='/classes'>Classes</Link></li>
-            <li><Link to='/trainer'>Trainer</Link></li> 
-            <li><Link to='/pricing'>Pricing</Link></li>
-            <li ><Link to='/joinUs' style={{color:"red",fontSize:"20px"}}>Join Us</Link></li>
+              <li><Link to='/aboutUs'>AboutUs</Link></li>
+              <li><Link to='/classes'>Classes</Link></li>
+              <li><Link to='/trainer'>Trainer</Link></li> 
+              <li><Link to='/pricing'>Pricing</Link></li>
+              <li ><Link to='/joinUs' style={{color:"red",fontSize:"20px"}}>Join Us</Link></li>
 
-          </ul>
-        </nav>
-      </header>
+            </ul>
+        
+        <button className={styles.menutoggle} onClick={handleClick}>
+          {isMenuOpen ? <FontAwesomeIcon icon={faXmark} />: <FontAwesomeIcon icon={faBars} />}
+        </button>
+        </header>
+       
     </div>
-
-
   )
 }
-export default Navbar
+export default Header
